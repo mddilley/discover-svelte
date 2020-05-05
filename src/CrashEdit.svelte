9b9e116f622506206aa22db87e3e32238540839d
Mike Dilley <mike.dilley@gmail.com>
2020-05-05 09:21:46 -0500

    Add CrashEdit mutation

--- src/CrashEdit.svelte
+++ src/CrashEdit.svelte
@@ -1,19 +1,17 @@
 <script>
   import { gql } from "apollo-boost";
   export let id;
-  const crashQuery = gql`
-    query GetCrashe {
-      atd_txdot_crashes(where: { crash_id: { _eq: ${id} } }) {
-        crash_id
-        case_id
-        crash_date
-        address_confirmed_primary
-        sus_serious_injry_cnt
-        death_cnt
+  const crashMutation = gql`
+    mutation update_atd_txdot_crashes($crashId: Int, $address: String) {
+      update_atd_txdot_crashes(
+        where: { crash_id: { _eq: $crashId } }
+        _set: { address_confirmed_primary: $address }
+      ) {
+        affected_rows
       }
     }
   `;
-  import { getClient, query } from "svelte-apollo";
+  import { getClient, mutate } from "svelte-apollo";
   import {
     Container,
     Form,
@@ -26,7 +24,18 @@
   const client = getClient();
 
   export let crashDetails;
-  let address = crashDetails.address_confirmed_primary;
+  let crashAddress = crashDetails.address_confirmed_primary;
+
+  async function updateCrash() {
+    try {
+      await mutate(client, {
+        mutation: crashMutation,
+        variables: { crashId: id, address: crashAddress }
+      });
+    } catch (error) {
+      // TODO
+    }
+  }
 </script>
 
 <Container class="text-center">
@@ -34,8 +43,12 @@
   <Form>
     <FormGroup>
       <Label for="exampleEmail">Primary Address</Label>
-      <Input type="text" name="email" id="exampleEmail" value={address} />
+      <Input
+        type="text"
+        name="email"
+        id="exampleEmail"
+        bind:value={crashAddress} />
     </FormGroup>
   </Form>
-  <Button>Submit</Button>
+  <Button on:click={updateCrash}>Submit</Button>
 </Container>
